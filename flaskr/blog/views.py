00be6d59f739fd3b7f95d3041fec8ddf63c5c849
rob <thaty.poly@gmail.com>
2020-01-29 15:19:37 -0500

    Restructured the flask form project based on pallets sqlalchemy setup at https://github.com/pallets/flask-sqlalchemy/tree/master/examples/flaskr

--- flaskr/blog/views.py
+++ flaskr/blog/views.py
@@ -1,44 +1,19 @@
-from flask import Flask, render_template, request, url_for, redirect
-from flask_sqlalchemy import SQLAlchemy
-from datetime import datetime
-from flask_migrate import Migrate
-from flask_wtf import FlaskForm
+from flask import Blueprint
+from flask import flash
+from flask import g
+from flask import redirect
+from flask import render_template
+from flask import request
+from flask import url_for
+from werkzeug.exceptions import abort
 
-app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///test.db'
-app.config['SECRET_KEY'] = 'secret'
-db = SQLAlchemy(app)
-migrate = Migrate(app,db)
+from flaskr import db
+from flaskr.blog.models import Design, Tag
 
-subs = db.Table('subs',
-    db.Column('design_id', db.Integer, db.ForeignKey('design.design_id')),
-    db.Column('tag_id', db.Integer, db.ForeignKey('tag.tag_id'))
-)
+bp: Blueprint = Blueprint("blog", __name__)
 
 
-class Design (db.Model):
-    design_id = db.Column(db.Integer, primary_key=True)
-    design_name = db.Column(db.String(40))
-    design_content = db.Column(db.String(200))
-    date_created = db.Column(db.DateTime, default= datetime.utcnow)
-    subscriptions = db.relationship('Tag', 
-    secondary=subs,
-    backref = db.backref('subscribers', lazy='dynamic')
-        )
-
-    def __repr__(self):
-        return '<Design {}>'.format(self.design_id)
-
-
-class Tag(db.Model):
-    tag_id = db.Column(db.Integer, primary_key=True)
-    tag_name = db.Column(db.String(20))
-
-    def __repr__(self):
-        return '<Tag {}>'.format(self.tag_name)
-
-
-@app.route("/", methods=['POST', 'GET'])
+@bp.route("/", methods=['POST', 'GET'])
 def index():
     if request.method == 'POST':
         tag_id_list = request.form.getlist('mycheckbox')
@@ -48,13 +23,14 @@ def index():
             for post in tag.subscribers:
                 filter_posts.append(post)
         tags = Tag.query.all()
-        return render_template('index.html', posts=filter_posts, tags=tags, filtered_tags=tag_id_list)
+        return render_template("blog/index.html", posts=filter_posts, tags=tags, filtered_tags=tag_id_list)
     else:
         posts = Design.query.all()
         tags = Tag.query.all()
-        return render_template('index.html', posts=posts, tags=tags)
+        return render_template("blog/index.html", posts=posts, tags=tags)
 
-@app.route('/delete/<int:id>')
+
+@bp.route('/delete/<int:id>')
 def delete(id):
     design_to_delete = Design.query.get_or_404(id)
 
@@ -73,12 +49,12 @@ def delete(id):
     try:
         db.session.delete(design_to_delete)
         db.session.commit()
-        return redirect('/')
+        return redirect(url_for("blog.index"))
     except:
         return 'There was a problem deleting that task'
 
 
-@app.route('/update/<int:id>',methods=['GET', 'POST'])
+@bp.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
     posts = Design.query.get_or_404(id)
     tag_string = ""
@@ -87,7 +63,7 @@ def update(id):
         tag_string += design_tag.tag_name + ","
         orignal_tags.add(design_tag.tag_name)
     if request.method == 'POST':
-        posts.design_content = request.form['content']
+        posts.design_content = request.form['blog']
         posts.design_name = request.form['name']
         tag_content = request.form['tag_input']
         multi_tags = tag_content.split(',')
@@ -123,26 +99,25 @@ def update(id):
             #Do nothing
         try:
             db.session.commit()
-            return redirect('/')
+            return redirect(url_for("blog.index"))
         except:
             return 'There was an error updating task'
     else:
-        return render_template('update.html', posts=posts, tag_string=tag_string)
+        return render_template("blog/index.html", posts=posts, tag_string=tag_string)
 
 
-@app.route('/search/<int:id>', methods=['GET', 'POST'])
+@bp.route('/search/<int:id>', methods=['GET', 'POST'])
 def search(id):
     posts = []
     tag = Tag.query.get_or_404(id)
     for p in tag.subscribers:
         posts.append(p)
 
-    return render_template('index.html', posts=posts)
+    return render_template("blog/index.html", posts=posts)
 
 
-
-@app.route('/adddesign', methods=['GET', 'POST'])
-def adddesign():
+@bp.route('/create', methods=['GET', 'POST'])
+def create():
     if request.method == 'POST':
         design_content_input = request.form['design_input']
         tag_content = request.form['tag_input']
@@ -164,13 +139,8 @@ def adddesign():
         try:
             db.session.add(new_design)
             db.session.commit()
-            return redirect('/')
+            return redirect(url_for('blog.index'))
         except:
             return "There was a problem adding new designs"
     else:
-        return render_template('adddesign.html')
-
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
+        return render_template("blog/create.html")
